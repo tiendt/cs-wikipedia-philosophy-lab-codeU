fed3ecc6ad7840d795c70af527ae89431b318113
tiendt <dao23t@mtholyoke.edu>
2016-06-12 22:29:47 -0400

    Done.

--- javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
+++ javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
@@ -9,43 +9,106 @@ import org.jsoup.nodes.Node;
 import org.jsoup.nodes.TextNode;
 
 import org.jsoup.select.Elements;
+import java.util.Stack;
 
 public class WikiPhilosophy {
-	
+
 	final static WikiFetcher wf = new WikiFetcher();
-	
+	final static List<String> allLinks = new ArrayList<String>();
+	final static Stack<Character> stack = new Stack<Character>();
+
 	/**
-	 * Tests a conjecture about Wikipedia and Philosophy.
-	 * 
-	 * https://en.wikipedia.org/wiki/Wikipedia:Getting_to_Philosophy
-	 * 
-	 * 1. Clicking on the first non-parenthesized, non-italicized link
-     * 2. Ignoring external links, links to the current page, or red links
-     * 3. Stopping when reaching "Philosophy", a page with no links or a page
-     *    that does not exist, or when a loop occurs
-	 * 
-	 * @param args
-	 * @throws IOException
-	 */
+	* Tests a conjecture about Wikipedia and Philosophy.
+	*
+	* https://en.wikipedia.org/wiki/Wikipedia:Getting_to_Philosophy
+	*
+	* 1. Clicking on the first non-parenthesized, non-italicized link
+	* 2. Ignoring external links, links to the current page, or red links
+	* 3. Stopping when reaching "Philosophy", a page with no links or a page
+	*    that does not exist, or when a loop occurs
+	*
+	* @param args
+	* @throws IOException
+	*/
 	public static void main(String[] args) throws IOException {
-		
-        // some example code to get you started
+		// some example code to get you started
 
-		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
-		Elements paragraphs = wf.fetchWikipedia(url);
+		String startUrl = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+		String endUrl = "https://en.wikipedia.org/wiki/Philosophy";
+		getEndUrl (startUrl, endUrl);
+	}
 
-		Element firstPara = paragraphs.get(0);
-		
-		Iterable<Node> iter = new WikiNodeIterable(firstPara);
-		for (Node node: iter) {
-			if (node instanceof TextNode) {
-				System.out.print(node);
+	public static void getEndUrl (String start, String end) throws IOException {
+		String link = start;
+
+		for (int i = 0; i < 10; i++) {
+			if (allLinks.contains(link)) {
+				System.err.println ("Link visited");
 			}
-        }
+			else {
+				allLinks.add(link);
+			}
+			Element element = getFirst(link);
+			if (element == null) {
+				System.out.println ("null link");
+			}
+			else {
+				System.out.println ("first: " + element.text());
+				link = "https://en.wikipedia.org" + element.attr("href");
+				if (link.equals(end)) {
+					System.out.println ("Found Philosophy");
+					break;
+				}
+			}
+		}
+	}
 
-        // the following throws an exception so the test fails
-        // until you update the code
-        String msg = "Complete this lab by adding your code and removing this statement.";
-        throw new UnsupportedOperationException(msg);
+	public static Element getFirst (String url) throws IOException {
+		Elements paragraphs = wf.fetchWikipedia(url);
+		return findFirstLink (paragraphs);
+	}
+
+	public static Element findFirstLink (Elements paragraphs) {
+		for (Element paragraph: paragraphs) {
+			Iterable<Node> iter = new WikiNodeIterable(paragraph);
+			for (Node node: iter) {
+				if (node instanceof TextNode) {
+					System.out.println (node);
+					checkParenthesis (((TextNode) node).text());
+				}
+				if (node instanceof Element) {
+					Element newNode = (Element) node;
+					if (newNode.tagName().equals("a") && stack.isEmpty()) {
+						Elements parents = newNode.parents();
+						if (!(parents.hasAttr("i") && parents.hasAttr("em"))) {
+							return newNode;
+						}
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+	public static void checkParenthesis(String str)
+	{
+		if (str.isEmpty()) {
+			System.out.println ("no string inserted");
+		}
+		for (int i = 0; i < str.length(); i++) {
+			char current = str.charAt(i);
+			if (current == '(') {
+				stack.push(current);
+			}
+			if (current == ')') {
+				char last = stack.peek();
+				if (current == ')' && last == '(') {
+					stack.pop();
+				}
+				else {
+					System.out.println ("unbalanced");
+				}
+			}
+		}
 	}
 }
